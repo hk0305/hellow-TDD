89b77497bd077f4624f124e45ce04f4914dd5a8e
hk0305 <hyounkeun2009@gmail.com>
2021-06-01 20:33:48 +0900

    create Product Model Test1

--- test/unit/products.test.js
+++ test/unit/products.test.js
@@ -11,11 +11,20 @@
 
 // * 함수 가져오기
 const productController = require('../../controller/products');
+const productModel = require('../../models/Product');
+
+// Mock 함수를 사용한 모델 생성 및 스파이 역할
+productModel.create = jest.fn();
 
 // * Create Function이 있는지 테스트
 describe("Product Controller Create", () => {
     // 테스트에 대한 설명
     it("should have a createProduct function", () => {
         expect(typeof productController.createProduct).toBe("function");
+    });
+    // Product를 DB에 넣기
+    it("should call ProductModel.create", () => {
+        productController.createProduct();
+        expect(productModel.create).toBeCalled();
     });
 });
