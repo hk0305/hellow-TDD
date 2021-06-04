67bd3f0bd1f9b5b56989a2794ba568e65962ce3c
hk0305 <hyounkeun2009@gmail.com>
2021-06-04 23:52:57 +0900

    Postman Create test1

--- controller/products.js
+++ controller/products.js
@@ -8,5 +8,6 @@ const productModel = require('../models/Product');
 exports.createProduct = (req, res, next) => {
     const createdProduct = productModel.create(req.body);
     // 201 response Code와 json Data를 응답한다.
+    console.log('createdProduct', createdProduct);
     res.status(201).json(createdProduct);
 };
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const productController = require("../controller/products")
+const productController = require("../controller/products");
 
 // productController는 middleware
 // middleware 함수를 url에 맵핑해준다.
-router.get('/', productController.hello );
+// router.get('/', productController.hello );
+
+router.post('/', productController.createProduct);
 
 module.exports = router;
